7c6c07562b91d483101e0f60361ee6d06e29a7eb
juanferm0410 <juanferm0410@gmail.com>
2025-10-14 21:59:04 -0500

    implementación de exception filter y formato hall

--- main.ts
+++ main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from 'src/common/filters/all-exception.filter';
+import { LoggingInterceptor } from './src/common/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+    app.useGlobalFilters(new AllExceptionsFilter());
+    app.useGlobalInterceptors(new LoggingInterceptor());
   await app.listen(3000);
   console.log(`🚀 Server running on ${process.env.FRONTEND_URL}`);
 }
---
+++ src/common/filters/all-exception.filter.ts
@@ -0,0 +1,45 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch() // Captura todas las excepciones
+export class AllExceptionsFilter implements ExceptionFilter {
+  private readonly logger = new Logger(AllExceptionsFilter.name);
+
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    let status: number;
+    let message: any;
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      const res = exception.getResponse();
+      message = typeof res === 'string' ? { message: res } : (res as object);
+    } else {
+      status = HttpStatus.INTERNAL_SERVER_ERROR;
+      message = { message: 'Internal server error' };
+    }
+
+    // Log del error (útil para desarrollo)
+    this.logger.error(
+      `Status: ${status} | Path: ${request.url} | Error: ${JSON.stringify(message)}`
+      //(exception as any).stack, // Da el mensaje de error completo
+    );
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      ...message,
+    });
+  }
+}
